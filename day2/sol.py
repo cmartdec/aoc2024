f6c9ae5ae8f10be8638abcf55a1c81be8e4bca0e
cmartdec <cmartdec@gmail.com>
2024-12-03 15:24:08 +0100

    day2 completed

--- day2/sol.py
+++ day2/sol.py
@@ -4,7 +4,20 @@ lines = D.split("\n")
 
 ans = 0
 
+#helper function
+
+def isSafe(level):
+	inc_or_dec = (level==sorted(level) or level==sorted(level, reverse=True))
+	ok = True
+	for i in range(len(level) - 1):
+		diff = abs(level[i] - level[i+1])
+		if not 1 <= diff <= 3:
+			ok = False
+	if inc_or_dec and ok:
+			return True
+
 # part one
+
 
 for line in lines:
 	xs = list(map(int, line.split()))
@@ -16,8 +29,18 @@ for line in lines:
 			ok = False
 	if inc_or_dec and ok:
 			ans += 1
-print(ans)
-			
+
+
+#part two
+ans = 0
+for line in lines:
+	xs = list(map(int, line.split()))
+	for i in range(len(xs)):
+		n = xs.pop(i)
+		if isSafe(xs):
+			ans += 1
+			break
+		xs.insert(i, n)
 
 
 
